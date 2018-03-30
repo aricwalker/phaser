35c36dfc48538297a1c2faa6c79993ce0554ded3
Aric Walker <aricwalker@github.com>
2018-03-29 18:40:34 -0600

    Paddle sprite is displaying
    Co-Authored-By: kristubbs <kristubbs@users.noreply.github.com>

--- dist/src/game.js
+++ dist/src/game.js
@@ -6,16 +6,21 @@ var game = new Phaser.Game(
   { preload: preload, create: create, update: update });
 
 function preload() {
+  game.load.atlas('breakout', 'assets/breakout-set.png', 'assets/breakout-set.json')
   game.load.image('background', 'assets/starfield-bg.jpg')
 }
 
 var background
+var paddle
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE)
-  background = game.add.sprite(0, 0,'background')
+  background = game.add.sprite(0, 0, 'background')
   background.width = 800
   background.height = 600
+
+  paddle = game.add.sprite(50, 500, 'breakout', 'paddle_blue.png')
+
 }
 
 function update() {
