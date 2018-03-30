c1d5c1fff1e8b4c082157dabfd5e338bbebfaea3
Aric Walker <aricwalker@github.com>
2018-03-29 18:21:42 -0600

    Added starfield background & image is scaled
    Updated Phaser lib to 2 instead of 3
    
    Co-Authored-By: kristubbs <kristubbs@users.noreply.github.com>

--- dist/src/game.js
+++ dist/src/game.js
@@ -1,9 +1,21 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(
+  800,
+  600,
+  Phaser.AUTO,
+  '',
+  { preload: preload, create: create, update: update });
 
 function preload() {
+  game.load.image('background', 'assets/starfield-bg.jpg')
 }
 
+var background
+
 function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE)
+  background = game.add.sprite(0, 0,'background')
+  background.width = 800
+  background.height = 600
 }
 
 function update() {
